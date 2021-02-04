fd2e639ff59248a198b0d787917d9a5229ab9b71
JEROME Emilien <jerome.emi@onja.org>
2021-02-04 15:48:39 +0300

    added react reducer

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-
+import store from './redux';
 function App(props) {
+	store.dispatch({type: 'INCREMENT'})
+	store.dispatch({type: 'DECREMENT'})
 	return <div></div>;
 }
 
--- redux/index.js
+++ redux/index.js
@@ -6,6 +6,7 @@ import redux, { createStore } from 'redux';
  * Read the comments below for the step-by-step challenges
  */
 
+
 // 1. Create action creators for having the count "increment" and "decrement"
 
 // 2. Create a reducer to handle your increment and decrement actions
@@ -15,3 +16,35 @@ import redux, { createStore } from 'redux';
 // 4. Set up the subscribe function so we can more easily see the changes to the Redux state as they happen
 
 // 5. Export the store as a default export
+
+function increment() {
+    return {
+        type: 'INCREMENT'
+    }
+}
+
+function decrement() {
+    return {
+        type: 'DECREMENT'
+    }
+}
+
+function reducer(count= 0, action) {
+    switch (action.type) {
+        case "INCREMENT":
+            return count + 1
+
+        case "DECREMENT":
+        return count - 1
+        default:
+            return count;
+    }
+}
+
+const store = createStore(reducer);
+
+store.subscribe(() => {
+    console.log(store.getState());
+})
+
+export default store;
